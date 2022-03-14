49d23734cd468a39551e3e8948b7e0ca0792c948
Nathan Phillip Brink <ohnobinki@ohnopublishing.net>
2022-03-14 03:39:53 +0000

    binki-microsoft-word-document-title-1.0

---
+++ README.md
@@ -0,0 +1,37 @@
+Update the browser’s title to match the Microsoft Word document’s title.
+
+## Problem
+
+When editing files in Microsoft Word Online using one’s [personal OneDrive](https://onedrive.live.com/) or [business OneDrive](https://office.live.com/start/OneDrive.aspx), Word updates the browser’s title to match the document:
+
+![Editing “Example Word Document.docx”.](https://i.imgur.com/C6jeCTt.png)
+
+This way, you can search for the document by name in your address bar:
+
+![Typing “example word” into the address bar.](https://i.imgur.com/0nSyjan.png)
+
+However, if you launched a Microsoft Word document through the [special WOPI protocol](https://docs.microsoft.com/en-us/microsoft-365/cloud-storage-partner-program/online/), Word does not do this.
+For example, a document named “Another Example.docx” just has a document title of “Word”:
+
+![Editing “Another Example.docx” opened via ShareFile](https://docs.microsoft.com/en-us/microsoft-365/cloud-storage-partner-program/online/)
+
+This makes searching for the document nearly impossible. For example, I have no results in my address bar:
+
+![Typing “another example” into the address bar.](https://i.imgur.com/erk5Z3r.png)
+
+## Solution
+
+This user script detects this situation and monitors the document’s title shown in the in-page blue header and copies the text to the document’s title.
+This updates the displayed value shown by the browser, making the tab searchable.
+
+However, this does not solve the problem completely.
+Every time you visit this document on a synced Firefox instance, Firefox will update the document title for that URI to the latest one it saw.
+So if you launch the document via Firefox for Android, which cannot run user scripts, you will overwrite the document’s history entry title to just be “Word”.
+To mitigate this issue, this script transparently adds an `x-title` GET parameter to the URI of the document.
+This provides two functionalities.
+During future loads of the document, the document’s title can be updated immediately upon navigating to the document—even before the document loads enough to populate the header within the document with the title and even if there is an issue with loading the document itself.
+Also, the address bar search can match the document title keywords in the URI itself, mitigating the fact that the title may have been overwritten by a device participating in Firefox Sync that does not have this userscript installed.
+
+## Installation
+
+[Install](binki-microsoft-word-document-title.user.js?raw=1)
---
+++ binki-microsoft-word-document-title.user.js
@@ -0,0 +1,52 @@
+// ==UserScript==
+// @name binki-microsoft-word-document-title
+// @version 1.0
+// @grant none
+// @author Nathan Phillip Brink (binki) (@ohnobinki)
+// @homepageURL https://github.com/binki/binki-microsoft-word-document-title/
+// @match https://word-edit.officeapps.live.com/we/wordeditorframe.aspx?*
+// @require https://github.com/binki/binki-userscript-delay-async/raw/252c301cdbd21eb41fa0227c49cd53dc5a6d1e58/binki-userscript-delay-async.js
+// @require https://github.com/binki/binki-userscript-when-element-changed-async/raw/88cf57674ab8fcaa0e86bdf5209342ec7780739a/binki-userscript-when-element-changed-async.js
+// ==/UserScript==
+(async () => {
+  const titleParamName = 'x-title';
+  // Extract the title from the URI before the document fully loads so we can set it more quickly
+  // if it is already set. This also sets the title if the document is in a broken/unauthenticated
+  // state (for Sharefile documents, consider using binki-sharefile-microsoft-word-recovery to automatically
+  // refresh these).
+  {
+    const url = new URL(location.href);
+    const title = url.searchParams.get(titleParamName);
+    if (title) {
+      document.title = title;
+    }
+  }
+  while (true) {
+    const throttlePromise = delayAsync(200);
+
+    const documentTitleSpan = document.querySelector('span[data-unique-id=DocumentTitleContent]');
+    if (documentTitleSpan) {
+      const title = documentTitleSpan.textContent;
+      // Put the title in a GET parameter in case if the page is loaded on a Greasemonkey-free
+      // copy of Firefox such as Firefox for Android which will overwrite the user’s history
+      // with an untitled enry, breaking address bar completion. Having it in the URI itself
+      // will mitigate this somewhat.
+      const url = new URL(location.href);
+      url.searchParams.set(titleParamName, title);
+      history.replaceState(history.state, title, url.toString());
+      document.title = title;
+      // Even though it is just text update, seems like it actually updates the text node itself.
+      // This doesn’t trigger characterData but does trigger subtree. So no point in trying to make
+      // this more specific—it is already specific to the node itself.
+      await whenElementChangedAsync(documentTitleSpan);
+    } else {
+      // Wait for this element to appear.
+      await whenElementChangedAsync(document.body);
+    }
+
+    // Don’t go too fast. If we received two mutation events in a row, wait a bit for the next
+    // one. If nothing had happened for a while, the delay will already be expired so this won’t
+    // hold anything up.
+    await throttlePromise;
+  }
+})();
